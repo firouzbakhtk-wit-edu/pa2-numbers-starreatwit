71c69ecc9e5d663cd120ea99f4dfa357c4dc5e60
ellie <ellie@10.220.84.121>
2024-10-10 20:34:19 -0400

    PA2-i am getting errors on junit but cannot figure out why

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,155 @@
+
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+
+
+import java.util.Scanner;
+/**
+ * Solution to PA2a. When the program runs, it prompts the user to enter five whole numbers.
+ * The program then outputs the sum of the positive numbers, sum of the negative numbers, sum of all five numbers, average of positive numbers, average of negative numbers, and the average of all five numbers.
+ * @author Ellie Starr
+ */
 public class PA2a {
 
-	// TODO: document this function
+	/**
+	 * Starts program, outputs prompted entry
+	 * 
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		//
+		Scanner input = new Scanner(System.in);
+		int a;
+		int b; 
+		int c;
+		int d;
+		int e;
+		
+		int negSum=0;
+		int posSum=0;
+		
+		int count = 0;
+		
+
+		
+	
+		
+		System.out.printf("Enter five whole numbers: ");
+		a=input.nextInt();
+		if (a<0) {
+			negSum = negSum+a;
+			count = count+1;}
+			else {
+				posSum = posSum+a;}
+		b=input.nextInt();
+		if (b<0) {
+			negSum = negSum+b;
+			count = count+1;}
+			else {
+				posSum = posSum+b;
+		}
+		c=input.nextInt();
+		if (c<0) {
+			negSum = negSum+c;
+			count = count+1;}
+			else {
+				posSum = posSum+c;
+		}
+		d=input.nextInt();
+		if (d<0) {
+			negSum = negSum+d;
+			count = count+1;}
+			else {
+				posSum = posSum+d;
+		}
+		e=input.nextInt();
+		if (e<0) {
+			negSum = negSum+e;
+			count = count+1;}
+			else {
+				posSum = posSum+e;
+		}
+		
+		double avePos = 0;
+		if(((5.0)-count) == 0) {
+			avePos=0;
+		}
+			else {
+				 avePos = posSum/((5.0)-count);
+			}
+		
+		
+		
+		double aveNeg = 0;
+		if (count==0) {
+			aveNeg = 0;
+		}
+		else {
+				aveNeg = negSum/count;
+				
+		}
+		
+		
+		int sumSum = negSum+posSum;
+		
+		
+		double aveSum = sumSum/(5.0);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	 if ((5.0-count)>1) {
+		System.out.printf("The sum of the %d positive numbers: %d%n", Math.round(5.0-count), posSum);
+	 }
+	 else if((5.0-count)==1) {
+		 System.out.printf("The sum of the %d positive number: %d%n", Math.round(5.0-count), posSum);
+	 }
+	 else {
+		 System.out.printf("There are no positive numbers%n");
+	 }
+	 
+	 if(count>1) {
+		 System.out.printf("The sum of the %d non-positive numbers: %d%n", Math.round(count), negSum);
+	 }
+	 else if(count==1) {
+		 System.out.printf("The sum of the %d non-positive number: %d%n", Math.round(count), negSum);
+	 }
+	 else {
+		 System.out.printf("There are no negative numbers%n");
+	 }
+		
+	 
+		System.out.printf("The sum of the 5 numbers: %d%n", sumSum);
+		
+		if((5.0-count)>1) {
+			System.out.printf("The average of the %d positive numbers: %.2f%n", Math.round(5.0-count), avePos);
+		}
+		else if((5.0-count)==0){
+			System.out.printf("There are no positive numbers%n");
+		}
+			else {
+			System.out.printf("The average of the %d positive number: %.2f%n", Math.round(5.0-count), avePos);
+			
+		}
+	
+		if(count>1) {
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", Math.round(count), aveNeg);
+		}
+		else if(count==0) {
+			System.out.printf("There are no negative numbers%n");
+		}
+		else {
+			System.out.printf("The average of the %d non-positive number: %.2f%n", Math.round(count), aveNeg);
+		}
+		
+		System.out.printf("The average of the 5 numbers: %.2f%n", aveSum);
+		
+		
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,64 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
+import java.util.Scanner;
+/**
+ *Solution to PA2b. When the program runs, it asks for values a b and c and then evaluates the quadratic formula.
+ * The program then prints the roots.
+ * @author Ellie Starr
+ */
 public class PA2b {
 
-	// TODO: document this function
+	/**
+	 * Starts program, outputs roots
+	 * 
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		//
+		Scanner input = new Scanner(System.in);
+		int a;
+		int b; 
+		int c;
+		
+		
+		System.out.printf("Enter a b c: ");
+		a=input.nextInt();
+		b=input.nextInt();
+		c=input.nextInt();
+		
+		double discriminant = ((b*b)-(4*a*c));
+		
+		double root = ((-b)/(2*a));
+		
+		double d = 0;
+		
+		double root1 = 0;
+		double root2 = 0;
+		
+		
+		if(discriminant<0) {
+			System.out.printf("Roots: imaginary%n");
+		}
+			else if(discriminant==0) {
+				System.out.printf("Root: %.2f%n", root);
+			}
+				else {
+					d = Math.sqrt(discriminant);
+					 root1 = ((-b)+(d))/(2*a);
+					 root2 = ((-b)-(d))/(2*a);
+					
+					if(root1<root2) {
+						System.out.printf("Roots: %.2f, %.2%n", root1, root2);
+					}
+					else if(root1>root2) {
+						System.out.printf("Roots: %.2f, %.2f%n", root2, root1);
+							
+					}
+				}
 	}
-
 }
+				
+				
+			
+		
+	
+
+
